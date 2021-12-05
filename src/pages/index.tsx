aa3a86b6e28798110bc5f2a420f2710a1e52cbc5
DinerIsmail <ismail.diner@gmail.com>
2021-12-05 19:38:41 +0000

    Sort out prettier and update formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,13 @@ import SomeImage from "components/SomeImage";
 import SomeText from "components/SomeText";
 
 const Home = () => {
-  return (
-    <Box mb={8} w="full">
-      <SomeText />
-      <SomeImage />
-      <CTASection />
-    </Box>
-  );
+	return (
+		<Box mb={8} w="full">
+			<SomeText />
+			<SomeImage />
+			<CTASection />
+		</Box>
+	);
 };
 
 export default Home;
