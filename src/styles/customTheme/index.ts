9a0b924231213083986246bcb8339e3a269ea9b7
DinerIsmail <ismail.diner@gmail.com>
2021-12-06 23:39:38 +0000

    Cleanup and set dark mode as default

--- src/styles/customTheme/index.ts
+++ src/styles/customTheme/index.ts
@@ -2,9 +2,11 @@ import { extendTheme } from '@chakra-ui/react';
 
 import colors from './colors';
 import Button from './components/button';
+import config from './config';
 import fonts from './fonts';
 
 const customTheme = extendTheme({
+	config,
 	fonts,
 	colors,
 	components: {
