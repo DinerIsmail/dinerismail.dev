58c4c79e2188aa1d2faa043562d5922609c49316
DinerIsmail <ismail.diner@gmail.com>
2022-01-21 13:24:31 +0000

    Minor copy changes

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,12 +3,14 @@ const defaultSEOConfig = {
 	title: 'Diner Ismail',
 	// titleTemplate: '%s | Diner Ismail',
 	defaultTitle: 'Diner Ismail',
-	description: 'Web developer trying to make the world a better place',
+	description:
+		'Full stack developer working on projects that make the world a better place',
 	canonical: 'https://dinerismail.dev',
 	openGraph: {
 		url: 'https://dinerismail.dev',
 		title: 'dinerismail.dev',
-		description: 'Web developer trying to make the world a better place',
+		description:
+			'Full stack developer working on projects that make the world a better place',
 		images: [
 			// {
 			// 	url: 'https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250',
